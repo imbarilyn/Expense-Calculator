e34d6628b5e3cc6d8b4af11e43a43793a1f02683
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-07 14:29:49 +0300

    created Expense component

---
+++ expense-calculator/src/componets/Expense.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// function Expense({expe}) {
+//   const {description, cost} = expe
+
+  
+//   return (
+  
+//        <tr>
+//       {/* <th scope="row">{key}</th> */}
+//       <td>{description}</td>
+//       <td>{cost}</td> 
+           
+//     </tr>     
+    
+//   )
+// }
+
+// export default Expense
